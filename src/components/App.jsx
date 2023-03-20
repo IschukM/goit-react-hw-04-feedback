07dd2d608cee8f3113eb0cfba3437374530fc241
IschukM <dzubak@ukr.net>
2023-03-20 20:26:10 +0200

    clone

---
+++ src/components/App.jsx
@@ -0,0 +1,64 @@
+import { Component } from 'react';
+import { Section } from './Section/Section';
+import { Feedback } from './Feedback/Feedback';
+import { Statistics } from './Statistics/Statistics';
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  stateIncrement = event => {
+    this.setState(prevState => {
+      switch (event.target.innerText) {
+        case 'Good':
+          return { good: prevState.good + 1 };
+        case 'Neutral':
+          return { neutral: prevState.neutral + 1 };
+        case 'Bad':
+          return { bad: prevState.bad + 1 };
+        default:
+          return;
+      }
+    });
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+  countPositiveFeedbackPercentage = () => {
+    return Math.round((this.state.good / this.countTotalFeedback()) * 100);
+  };
+
+  render() {
+    return (
+      <>
+        <Section title="Feedback">
+          <Feedback feedback={this.state} onIncrement={this.stateIncrement} />
+        </Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() ? (
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              total={this.countTotalFeedback()}
+              percentage={
+                !isNaN(this.countPositiveFeedbackPercentage())
+                  ? this.countPositiveFeedbackPercentage()
+                  : 0
+              }
+            />
+          ) : (
+            <div className="Container">
+              <p className="feedalert">There is no feedback</p>
+            </div>
+          )}
+        </Section>
+      </>
+    );
+  }
+}
